093e75146b532945eada84c9e9d47f4f666f2257
sri2403 <sriharinimuthukrishnan@gmail.com>
2024-06-11 19:53:51 +0530

    style updated

--- Components/Nav.jsx
+++ Components/Nav.jsx
@@ -6,20 +6,20 @@ const Nav = () => {
     return (
         <nav className="navbar navbar-expand-lg ">
             <div className="container-fluid">
-                <h1 className="navbar-brand" to="/">My App</h1>
+                <h1 className="navbar-brand text-white" to="/">My App</h1>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav p-3 ">
                         <li className="nav-item ">
-                            <Link className="nav-link" to="/">Home</Link>
+                            <Link className="nav-link text-white" to="/">Home</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/register">Register</Link>
+                            <Link className="nav-link text-white" to="/register">Register</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/login">Login</Link>
+                            <Link className="nav-link text-white" to="/login">Login</Link>
                         </li>
                     </ul>
                 </div>
